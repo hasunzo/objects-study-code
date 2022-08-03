d22c399c60abc79c726c197fe350752756feae7d
hasunzo <gktjsdl470@gmail.com>
2022-08-04 06:41:20 +0900

    캡슐화의 원칙에 따라 속성들을 외부로 노출해서는 안 된다

--- src/chapter3/DiscountCondition.java
+++ src/chapter3/DiscountCondition.java
@@ -10,5 +10,45 @@ public class DiscountCondition {
 
     private DayOfWeek dayOfWeek;    // 기간 조건에서만 사용
     private LocalTime startTime;    // 기간 조건에서만 사용
-    private LocalTime endTime;      // 기간 조건에서만 사용
+    private LocalTime endTime;      // 기간 조건에서만 사용스
+
+    public DiscountConditionType getType() {
+        return type;
+    }
+
+    public void setType(DiscountConditionType type) {
+        this.type = type;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 }
