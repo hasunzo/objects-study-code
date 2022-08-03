5993da58337083290680be18bfa042910f2f37c4
hasunzo <gktjsdl470@gmail.com>
2022-08-04 06:59:53 +0900

    ReservationAgency 클래스 구현하기

---
+++ src/chapter3/ReservationAgency.java
@@ -0,0 +1,46 @@
+package chapter3;
+
+public class ReservationAgency {
+    public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
+        Movie movie = screening.getMovie();
+
+        // 할인 될 수 있는 지 판단
+        boolean discountable = false;
+        for (DiscountCondition condition : movie.getDiscountConditions()) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                discountable = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
+                        condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
+                        condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
+            } else {
+                discountable = condition.getSequence() == screening.getSequence();
+            }
+
+            if (discountable) {
+                break;
+            }
+        }
+
+        // 할인 요금 계산 - 할인 정책의 타입에 따라 할인 요금을 계산하는 로직을 분기해야 한다
+        Money fee;
+        if (discountable) {
+            Money discountAmount = Money.ZERO;
+            switch (movie.getMovieType()) {
+                case AMOUNT_DISCOUNT:
+                    discountAmount = movie.getDiscountAmount();
+                    break;
+                case PERCENT_DISCOUNT:
+                    discountAmount = movie.getFee().times(movie.getDiscountPercent());
+                    break;
+                case NONE_DISCOUNT:
+                    discountAmount = Money.ZERO;
+                    break;
+            }
+
+            fee = movie.getFee().minus(discountAmount);
+        } else {
+            fee = movie.getFee();
+        }
+
+        return new Reservation(customer, screening, fee, audienceCount);
+    }
+}
