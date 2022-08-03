ff7b878cfda2b5b01854be65cd68fc851beb49fe
hasunzo <gktjsdl470@gmail.com>
2022-08-04 06:40:28 +0900

    할인 조건을 구현하는 데 필요한 데이터(2) - 할인 조건을 구현하는 클래스

--- src/chapter3/DiscountCondition.java
+++ src/chapter3/DiscountCondition.java
@@ -1,4 +1,14 @@
 package chapter3;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
 public class DiscountCondition {
+    private DiscountConditionType type;
+
+    private int sequence;           // 순번 조건에서만 사용
+
+    private DayOfWeek dayOfWeek;    // 기간 조건에서만 사용
+    private LocalTime startTime;    // 기간 조건에서만 사용
+    private LocalTime endTime;      // 기간 조건에서만 사용
 }
