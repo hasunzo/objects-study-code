82595efeedf2c5cc752be88add9c75685f3a8999
hasunzo <gktjsdl470@gmail.com>
2022-08-04 13:26:33 +0900

    Screening 리팩토링
    - Movie가 금액 할인 정책이나 비율 할인 정책을 지원할 경우
    Movie의 isDiscountable 메서드 호출해서 여부 판단

--- src/chapter3/Screening.java
+++ src/chapter3/Screening.java
@@ -30,4 +30,22 @@ public class Screening {
     public void setWhenScreened(LocalDateTime whenScreened) {
         this.whenScreened = whenScreened;
     }
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
 }
