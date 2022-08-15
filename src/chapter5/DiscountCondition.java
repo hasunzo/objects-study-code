eaa00692cffa635c333084c6ae402b5702ae98b3
hasunzo <gktjsdl470@gmail.com>
2022-08-15 18:21:47 +0900

    chapter5. DiscountCondition 의 문제점
    1. 새로운 할인 조건이 추가 될 경우 구현이 변경
    2. 기간 조건에 대한 요구사항이 달라질 경우 구현이 변경
    3. 순번 조건에 대한 요구사항이 달라질 경우 구현이 변경

--- src/chapter5/DiscountCondition.java
+++ src/chapter5/DiscountCondition.java
@@ -11,6 +11,7 @@ public class DiscountCondition {
     private LocalTime startTime;
     private LocalTime endTime;
 
+    // 새로운 할인 조건이 추가 될 경우 구현이 변경
     public boolean isSatisfiedBy(Screening screening) {
         if (type == DiscountConditionType.PERIOD) {
             return isSatisfiedByPeriod(screening);
@@ -19,12 +20,14 @@ public class DiscountCondition {
         return isSatisfiedBySequence(screening);
     }
 
+    // 기간 조건에 대한 요구사항이 달라질 경우 구현이 변경
     private boolean isSatisfiedByPeriod(Screening screening) {
         return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
                 startTime.compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
                 endTime.isAfter(screening.getWhenScreened().toLocalTime());
     }
 
+    // 순번 조건에 대한 요구사항이 달라질 경우 구현이 변경
     private boolean isSatisfiedBySequence(Screening screening) {
         return sequence == screening.getSequence();
     }
